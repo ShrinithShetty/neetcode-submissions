1ea572a69c6de6625a2fc5ab298c3eaf94533392
Shrinith Shetty <122169413+ShrinithShetty@users.noreply.github.com>
2026-05-26 22:05:35 +0530

    Add: validate-parentheses - submission-14

---
+++ Data Structures & Algorithms/validate-parentheses/submission-14.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        hashmap = {']':'[','}':'{',')':'('}
+        stk = []
+
+        for i in s:
+            if i not in hashmap:
+                stk.append(i)
+            else:
+                if not stk:
+                    return False
+                else:
+                    popped = stk.pop()
+                    if popped != hashmap[i]:
+                        return False
+        return not stk
+        
