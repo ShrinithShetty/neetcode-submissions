3b7a691beab03474b42d37ac29e1712986e2a05f
Shrinith Shetty <122169413+ShrinithShetty@users.noreply.github.com>
2026-06-01 22:07:54 +0530

    Add: minimum-window-with-characters - submission-29

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-29.py
@@ -0,0 +1,34 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        sf = {}
+        tf = {}
+
+
+        for c in t:
+            tf[c] = 1 + tf.get(c,0)
+
+        have, need = 0, len(tf)
+        res, reslen = [-1,-1], float('inf')
+
+        l = 0
+
+        for r in range(len(s)):
+            c = s[r]
+            sf[c] = 1 + sf.get(c,0)
+
+            if c in tf and sf[c] == tf[c]:
+                have += 1
+
+
+            while have == need:
+                if (r-l+1) < reslen:
+                    res = [l,r]
+                    reslen = r - l + 1
+
+                sf[s[l]] -= 1
+                if s[l] in tf and sf[s[l]] < tf[s[l]]:
+                    have -= 1
+                l += 1
+
+        l, r = res
+        return s[l:r+1] if reslen != float('inf') else ""
