c4a837271e4e60fef9a791453f092c7c144c5f79
Shrinith Shetty <122169413+ShrinithShetty@users.noreply.github.com>
2026-06-01 23:10:39 +0530

    Add: eating-bananas - submission-14

---
+++ Data Structures & Algorithms/eating-bananas/submission-14.py
@@ -0,0 +1,18 @@
+from math import ceil
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def k_works(k):
+            hours = 0
+            for pile in piles:
+                hours += ceil(pile/k)
+            return hours <= h
+
+
+        l, r = 1, max(piles)
+        while l < r:
+            m = (l+r)//2
+            if k_works(m):
+                r = m
+            else:
+                l = m + 1
+        return r
