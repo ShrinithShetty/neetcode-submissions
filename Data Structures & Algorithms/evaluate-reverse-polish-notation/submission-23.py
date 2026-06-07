5ad8bef223fe12ad0b7cd61fe622fcb48a695042
Shrinith Shetty <122169413+ShrinithShetty@users.noreply.github.com>
2026-06-08 00:26:41 +0530

    Add: evaluate-reverse-polish-notation - submission-23

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-23.py
@@ -0,0 +1,24 @@
+from math import ceil, floor
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stk = []
+        for s in tokens:
+            if s in "+-/*":
+                b, a = stk.pop(), stk.pop()
+
+                if s == "+":
+                    stk.append(a+b)
+                elif s == '-':
+                    stk.append(a-b)
+                elif s == '*':
+                    stk.append(a*b)
+                else:
+                    div = a/b
+                    if div < 0:
+                        stk.append(ceil(div))
+                    else:
+                        stk.append(floor(div))
+
+            else:
+                stk.append(int(s))
+        return stk[-1]
