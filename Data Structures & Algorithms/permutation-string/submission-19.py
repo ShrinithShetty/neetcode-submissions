45acba63fdb19f751d942559ebab7905bcc859eb
Shrinith Shetty <122169413+ShrinithShetty@users.noreply.github.com>
2026-06-13 23:53:41 +0530

    Add: permutation-string - submission-19

---
+++ Data Structures & Algorithms/permutation-string/submission-19.py
@@ -0,0 +1,25 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s2) < len(s1):
+            return False
+        f1 = [0]*26
+        f2 = [0]*26
+
+        for i in range(len(s1)):
+            f1[ord(s1[i])-97] += 1
+            f2[ord(s2[i])-97] += 1
+
+        if f1 == f2:
+            return True
+        
+        l = 0
+
+        for r in range(len(s1), len(s2)):
+            f2[ord(s2[r])-97] += 1
+            f2[ord(s2[l])-97] -= 1
+            l += 1
+
+            if f1 == f2:
+                return True
+
+        return False
