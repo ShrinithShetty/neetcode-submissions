57b1b1b517471a796ac9328237a102e042e4066d
Shrinith Shetty <122169413+ShrinithShetty@users.noreply.github.com>
2026-06-21 00:24:57 +0530

    Add: combinations-of-a-phone-number - submission-4

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-4.py
@@ -0,0 +1,21 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        res, sol = [], []
+
+        if digits == "":
+            return []
+
+        def backtrack(i):
+            if i == len(digits):
+                res.append(''.join(sol))
+                return
+            
+            letter_map = {'2':'abc','3':'def','4':'ghi','5':'jkl','6':'mno','7':'pqrs','8':'tuv','9':'wxyz'}
+            for letter in letter_map[digits[i]]:
+                sol.append(letter)
+                backtrack(i+1)
+                sol.pop()
+
+
+        backtrack(0)
+        return res
