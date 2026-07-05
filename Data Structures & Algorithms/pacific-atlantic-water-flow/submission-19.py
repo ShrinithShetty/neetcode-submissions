f9c6afc7108477bfa831c88ed6cd94fd6831e21c
Shrinith Shetty <122169413+ShrinithShetty@users.noreply.github.com>
2026-07-05 22:37:35 +0530

    Add: pacific-atlantic-water-flow - submission-19

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-19.py
@@ -0,0 +1,43 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        p_q = deque()
+        p_seen = set()
+        a_q = deque()
+        a_seen = set()
+
+        m = len(heights)
+        n = len(heights[0])
+
+        for j in range(n):
+            p_q.append([0,j])
+            p_seen.add((0,j))
+
+        for i in range(1, m):
+            p_q.append([i,0])
+            p_seen.add((i,0))
+
+        for i in range(m):
+            a_q.append([i,n-1])
+            a_seen.add((i,n-1))
+
+        for j in range(n-1):
+            a_q.append([m-1,j])
+            a_seen.add((m-1,j))
+
+        
+        def coord(q,s):
+            while q:
+                for _ in range(len(q)):
+                    i, j = q.popleft()
+
+                    for r,c in [(i+1,j),(i-1,j),(i,j+1),(i,j-1)]:
+                        if 0 <= r < m and 0 <= c < n and heights[r][c] >= heights[i][j] and (r,c) not in s:
+                            q.append([r,c])
+                            s.add((r,c))
+
+        coord(p_q, p_seen)
+        coord(a_q, a_seen)
+
+        return list(p_seen.intersection(a_seen))
+
+        
