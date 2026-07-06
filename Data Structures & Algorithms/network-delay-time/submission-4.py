c4a4214ed2a5e14742757e216673292c084fccf7
Shrinith Shetty <122169413+ShrinithShetty@users.noreply.github.com>
2026-07-06 23:06:48 +0530

    Add: network-delay-time - submission-4

---
+++ Data Structures & Algorithms/network-delay-time/submission-4.py
@@ -0,0 +1,24 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        graph = defaultdict(list)
+
+        for a, b, time in times:
+            graph[a].append((b,time))
+
+        min_time = {}
+        min_heap = [(0,k)]
+
+        while min_heap:
+            time_k_to_i, i = heapq.heappop(min_heap)
+            if i in min_time:
+                continue
+            min_time[i] = time_k_to_i
+            for nei, nei_time in graph[i]:
+                if nei not in min_time:
+                    heapq.heappush(min_heap, (time_k_to_i + nei_time, nei))
+
+        
+        if len(min_time) == n:
+            return max(min_time.values())
+        else:
+            return -1
