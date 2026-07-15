8b3e3965bc01c2b34a29a6a45c7344682f176552
Shrinith Shetty <122169413+ShrinithShetty@users.noreply.github.com>
2026-07-16 00:28:53 +0530

    Add: palindromic-substrings - submission-8

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-8.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        res = 0
+        for i in range(len(s)):
+            res += self.validPalind(s, i,i)
+            res += self.validPalind(s,i,i+1)
+
+        return res
+    def validPalind(self, s, l, r):
+            res = 0
+            while 0 <= l and r < len(s) and s[l] == s[r]:
+                res += 1
+                l -= 1
+                r += 1
+
+            return res
