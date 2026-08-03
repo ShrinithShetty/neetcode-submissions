d050fc822aad7c38c3a1b5c47eee93d7811d2b77
Shrinith Shetty <122169413+ShrinithShetty@users.noreply.github.com>
2026-08-03 21:26:40 +0530

    Add: jump-game-ii - submission-4

---
+++ Data Structures & Algorithms/jump-game-ii/submission-4.py
@@ -0,0 +1,14 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+
+        smallest = 0
+        far, end = 0, 0
+
+        for i in range(len(nums)-1):
+            far = max(far, nums[i] + i)
+            if i == end:
+                smallest += 1
+                end = far
+
+        return smallest
+        
